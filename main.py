76ad439fe8e9ad5b6d1c0bd4504597d214292f42
qhdgkdbs <qhdgkdbs@gmail.com>
2020-12-21 15:20:01 +0900

    auto shippping

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from selenium import webdriver
 import xlsxwriter
 from time import sleep
+import re
 
 # 크롬 드라이버 가져오기
 driver = webdriver.Chrome('/Users/bonghayun/Desktop/project/python/python_excel_miele/chromedriver')
@@ -18,11 +19,6 @@ data_format1 = workbook.add_format({'bg_color': '#E8F8FF', 'border': 7, 'bottom'
 #수집한 정보를 입력
 row = 1
 
-#배송비가 있는 상점들
-f = open("mall_with_shipping_price.txt", "r")
-mall_with_shippping_price = f.read().split('\n')
-f.close()
-
 #첫 줄 내용
 row_one_line = ["MODEL", "SP", "Promotion Price", "Differ",
                 "가격_1", "쇼핑물_1", "url_1", "상품명_1",
@@ -33,11 +29,8 @@ row_one_line = ["MODEL", "SP", "Promotion Price", "Differ",
                 "가격_6", "쇼핑물_6", "url_6", "상품명_6",
                 "가격_7", "쇼핑물_7", "url_7", "상품명_7",
                 "가격_8", "쇼핑물_8", "url_8", "상품명_8",
-                "가격_9", "쇼핑물_9", "url_9", "상품명_9"]
-
-#첫 줄에 데이터 입력
-for n, info in enumerate(row_one_line):
-    worksheet.write(0, n, info)
+                "가격_9", "쇼핑물_9", "url_9", "상품명_9",
+                "가격_10", "쇼핑물_10", "url_10", "상품명_10"]
 
 #첫 줄 형식지정 & 틀 고정
 worksheet.set_row(0, cell_format=data_format_first_line)
@@ -54,6 +47,9 @@ f.close()
 # url.txt에서 가져온 정보가 [제품명 가격1 가격2 url, 제품명 가격1 가격2 url, ...] 이런식으로 저장되어있음,
 # index에는 "제품명 가격1 가격2 url" 이렇게 들어있고
 for n,index in enumerate(lines):
+    #배송비를 가져오기 위한 0, 왜냐면 쇼핑물 모음집이 있으면 배열이 댕겨져서, 안맞아.
+    #그걸 보정하기 위해서 쇼핑물 모음집이 있으면 그 수 만큼 댕겨줄꺼
+    mall_group = 0
 
     # lines[0] 의 정보 중에서 불필요한 \n을 제거
     info_product = index.replace('\n','')
@@ -65,7 +61,7 @@ for n,index in enumerate(lines):
     driver.get(data[n][3])
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     #컴퓨터가 html파일을 전부 가져오지 못해서 html을 전부 가져오기 위해 0.1초의 시간을 줌!
-    sleep(0.1)
+    sleep(0.5)
     html = driver.page_source
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -74,8 +70,8 @@ for n,index in enumerate(lines):
     price_info_numbers = len(soup.find_all('div', class_='basicList_title__3P9Q7'))
 
     #데이터의 갯수를 제한하는 단계
-    if(price_info_numbers > 8):
-        price_info_numbers = 8
+    if(price_info_numbers > 10):
+        price_info_numbers = 10
     else:
         pass
 
@@ -85,7 +81,7 @@ for n,index in enumerate(lines):
         if (i % 2) != 0:
             worksheet.set_column(index, index + 3, cell_format=data_format1)
 
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>___" + data[n][0] + "___<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" )
+    # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>___" + data[n][0] + "___<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" )
 
     #크롤링한 정보를 담을 data_arr이라는 배열,
     #한 정보의 싸이클이 끝나고 다시 시작하면 초기화.
@@ -104,6 +100,10 @@ for n,index in enumerate(lines):
         url = metadata.a.get('href')
         # print("<url> : ", url)  # url
 
+#원래 배송비 가져오는 위치
+
+
+
         #위에서 크롤링한 html에서 정보 뺴오자
         if(soup.find_all('a', class_='basicList_mall__sbVax')[i].img):
             #일반적인 쇼핑물
@@ -120,20 +120,36 @@ for n,index in enumerate(lines):
 
             # data_arr.insert(len(data_arr), [int(price), "err" + mall_name, url, title])
 
+            # 해당 쇼핑물에 배송비가 있는지
+            # 배송비 > 배송정보 숫자만 받기 > 숫자 변수에 저장,
+            shipping_price = False
+            if(mall_name != '쇼핑몰별 최저가'):
+                shipping_price_box = soup.find_all('ul', class_='basicList_mall_option__1qEUo')[i - mall_group]
+                shipping_price = shipping_price_box.find_all('em', class_='basicList_option__3eF2s')[0].text.replace(',','')
 
-            if mall_name in mall_with_shippping_price:
+                if (re.search(r'\d+', shipping_price)):
+                    shipping_price = int(re.search(r'\d+', shipping_price).group())
+                    # print(shipping_price)
+                    pass
+                else:
+                    shipping_price = False
+            else:
+                #쇼핑물별 최저가인 경우
+                #n번째를 추가해서 배열 순서를 맞추자
+                mall_group = mall_group + 1
+
+
+            if shipping_price:
                 # 네이버 스토어 중에서 배송비가 있는 곳
-                data_arr.insert(len(data_arr),[int(price)+100000,"네이버["+mall_name + "]",url,title])
+                # print(data[n][0]+str(i) + mall_name + ' > ' + str(shipping_price))
+                data_arr.insert(len(data_arr),[int(price) + shipping_price, "네이버["+mall_name + "-배송비 포함]", url, title])
+
             else:
                 # 가격비교 원부 or 배송비 없는 네이버 스토어
                 if mall_name == '쇼핑몰별 최저가':
                     pass
                 else:
                     data_arr.insert(len(data_arr),[int(price),'네이버[' + mall_name + ']',url,title])
-
-
-
-
 
 
 
@@ -161,15 +177,18 @@ for n,index in enumerate(lines):
     row = row + 1
 
 
-
 #3번쨰 컬럼 서식 (differ 값 구하는 식)
-for row_num in range(1, 61):
+for row_num in range(1, 100):
     worksheet.write_formula(row_num-1, 3,
                             '= -$C%d + $E%d' % (row_num, row_num))
 
 #differ값이 0보다 작으면 아래 형식 지정
 differ_smaller_than_zero = workbook.add_format({'bg_color': '#FFC7CE',
                                'font_color': '#9C0006'})
+
+#첫 줄에 데이터 입력
+for n, info in enumerate(row_one_line):
+    worksheet.write(0, n, info)
 
 worksheet.conditional_format('D2:D61', {'type': 'cell',
                                          'criteria': '<',
